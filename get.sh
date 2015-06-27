1495f2a6b61d79c4623f921ced0fae982e08163b
billblakney <b.blakney@cox.net>
2015-06-27 17:06:30 -0400

    modify get and put to take multiple dot.* args

--- get.sh
+++ get.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 ############################
-# put
-# This script copies the specified local file to the dotted file
-# in the parent directory.
+# Copy home directory files to dotfiles.
 ############################
 if [ "$#" -lt 1 ]; then
-    echo "Usage: ${0##*/} bashrc ..."
+    echo "Usage: ${0##*/} dot.bashrc ..."
     exit 1
 fi
 
+# Regular expression for ./dot.*
+re='dot\.(.*)'
+
 for arg in "$@"
 do
-   root=$arg
-   localname="dot.$arg"
-   homename="../.$arg"
-
-   if [ -e $homename ]; then
-      echo cp $homename $localname
-      cp $homename $localname
+   localname="$arg"
+   if [[ $localname =~ $re ]]; then
+      root="${BASH_REMATCH[1]}";
+      homename="../.$root"
+      if [[ -e $homename ]]; then
+         echo cp $homename $localname
+         cp $homename $localname
+      else
+         echo "WARNING: File not found: $homename"
+      fi
    else
-      echo "WARNING: No dotfile for $arg"
+      echo "WARNING: Invalid request: $localname"
    fi
 done
--- put.sh
+++ put.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 ############################
-# put
-# This script copies the specified local file to the dotted file
-# in the parent directory.
+# Copy dotfiles to home directory.
 ############################
 if [ "$#" -lt 1 ]; then
-    echo "Usage: ${0##*/} bashrc ..."
+    echo "Usage: ${0##*/} dot.bashrc ..."
     exit 1
 fi
 
+# Regular expression for ./dot.*
+re='dot\.(.*)'
+
 for arg in "$@"
 do
-   root=$arg
-   localname="dot.$arg"
-   homename="../.$arg"
-
-   if [ -e $localname ]; then
-      echo cp $localname $homename
-      cp $localname $homename
+   localname="$arg"
+   if [[ $localname =~ $re ]]; then
+      root="${BASH_REMATCH[1]}";
+      homename="../.$root"
+      if [[ -e $localname ]]; then
+         echo cp $localname $homename
+         cp $localname $homename
+      else
+         echo "WARNING: File not found: $localname"
+      fi
    else
-      echo "WARNING: No dotfile for $arg"
+      echo "WARNING: Invalid request: $localname"
    fi
 done
