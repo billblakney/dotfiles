093f1f6678c28cd715ddab9f33dfca53df01a612
billblakney <b.blakney@cox.net>
2015-06-27 20:22:05 -0400

    update dif.sh to take any number of args, incl. zero; dot. prefix optional

--- dif.sh
+++ dif.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
-#
-# Get list of all dot.* files.
-list=($(find . -name 'dot.*'))
-#
-# Regular expression for ./dot.*
-re='\.\/dot(.*)'
-#
-for i in ${list[@]};
+
+function usage {
+   echo "Usage: ${0##*/} [<dotfile> [<dotfile> ...]]"
+   echo "  Use zero args to diff all dotfiles"
+   echo "  The format of a dotfile is \"dot.*\""
+   exit 1
+}
+
+function error {
+   echo $1
+}
+
+if [ "$#" -eq 1 ] && [ "$1" == "-h" ]
+then
+   usage
+fi
+
+if [ "$#" -eq 0 ]; then
+   dotfiles=($(find . -name 'dot.*'))
+else
+   dotfiles=( "$@" )
+fi
+
+regex='dot\.(.*)'
+
+for dotfile in "${dotfiles[@]}"
 do
-   if [[ $i =~ $re ]]; then
-      echo diff $i ../${BASH_REMATCH[1]};
-      diff $i ../${BASH_REMATCH[1]};
+   if [[ $dotfile =~ $regex ]]; then
+      root="${BASH_REMATCH[1]}"
+   else
+      root=$dotfile
+   fi
+
+   localname=dot.$root
+   homename=../.$root
+
+   if [[ ! -e $localname ]]; then
+      echo "ERROR: Couldn't find $localname"
+   elif [[ ! -e $homename ]]; then
+      echo "ERROR: Couldn't find $homename"
+   else
+      echo diff $localname $homename
+      diff $localname $homename
    fi
 done
