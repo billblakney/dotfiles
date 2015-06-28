7b56c3cf9f02cf39ab47720ca0bfce41263ea363
Bill Blakney <b.blakney@cox.net>
2015-06-27 22:37:44 -0400

    add -q arg to dif.sh and add framework for processing cmd line args

--- dif.sh
+++ dif.sh
@@ -3,6 +3,8 @@
 # Diff local dotfiles with home directory.
 ############################
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function usage {
    echo "Usage: ${0##*/} [<dotfile> [<dotfile> ...]]"
    echo "  Use zero args to diff all dotfiles"
@@ -10,25 +12,56 @@ function usage {
    exit 1
 }
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function error {
    echo $1
 }
 
-if [ "$#" -eq 1 ] && [ "$1" == "-h" ]
-then
-   usage
-fi
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# main
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+args=($@)
 
-if [ "$#" -eq 0 ]; then
+# command-line flags
+qregex='-q'
+hregex='-h'
+
+# command-line settings
+qflag=""
+
+#-------------------------
+# process command-line args
+#-------------------------
+for arg in "${args[@]}"
+do
+   if [[ $arg =~ $hregex ]]; then
+      usage
+      exit 1
+   elif [[ $arg =~ $qregex ]]; then
+      qflag="-q"
+      args=(${args[@]/$qflag/})
+   fi
+done
+
+#-------------------------
+# set dotfiles list
+#-------------------------
+if [ "${#args[@]}" -eq 0 ]; then
    dotfiles=($(find . -name 'dot.*'))
 else
-   dotfiles=( "$@" )
+   dotfiles=( "${args[@]}" )
 fi
 
+#-------------------------
+# process dotfiles
+#-------------------------
 regex='dot\.(.*)'
 
 for dotfile in "${dotfiles[@]}"
 do
+   # Real dotfiles begin with "dot." prefix, but can be specified
+   # on the command-line arguments with or without that prefix.
    if [[ $dotfile =~ $regex ]]; then
       root="${BASH_REMATCH[1]}"
    else
@@ -43,7 +76,7 @@ do
    elif [[ ! -e $homename ]]; then
       echo "ERROR: Couldn't find $homename"
    else
-      echo diff $localname $homename
-      diff $localname $homename
+      #echo diff $qflag $localname $homename
+      diff $qflag $localname $homename
    fi
 done
